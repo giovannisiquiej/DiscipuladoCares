1745cf3714b61072a9562aa497f440bd58e5fe61
Giovanni Siquiej <giovannisiquiej@hotmail.com>
2023-10-09 10:14:35 -0600

    incluye cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 //importa los routes de cada entidad
 import iglesiasRoutes from "./routes/iglesia.routes.js";
 import ministrosRoutes from "./routes/ministro.routes.js";
+//const cors = require("cors");
 
 //ejecuta el App
 const app = express();
 
+// const corsOpt = {
+//   origin: /\.onrender\.com$/,
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// };
+
 //middlewares
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.use(iglesiasRoutes);
